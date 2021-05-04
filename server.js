ffdd127f602ba79608fe52a123f2a559af7fbe83
kedar sedai <kedar.sedai05@gmail.com>
2021-05-04 21:40:11 +0545

    DB Connection success

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyparser  = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+//Middleware
+// app.use(bodyparser.urlencoded({extended:false}));
+// app.use(bodyparser.json());
+
+//Import route
+const bookRoute = require('./routes/bookRoutes');
+
+//Middleware
+app.use('/api/v1', bookRoute);
+
+//db connection
+mongoose
+    .connect(process.env.DB,{useNewUrlParser:true, useUnifiedTopology:true})
+    .then(() => console.log('Connected to database!!'))
+    .catch(err => console.log(err));
+
+app.get('/', (req, res) =>{
+    res.send("Hello from Server");
+});
+
+app.listen(port, () => console.log(`Server is running at ${port}`));
